47bbe0ea2ec94dd451535a7e5503b279f2dcca74
Kostas Kontaxis <kkontaxis@pm.me>
2021-01-06 17:46:58 +0200

    fixed install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/bash
 
 list=("alacritty/" "awesome/" "fish/" "nvim/" "rofi/")
-for dir in ${list[@]}; do ln -s $dir ../$dir ; done
+for dir in ${list[@]}; do ln -s BetterThanYour/$dir ~/.config ; done
