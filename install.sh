a72a69e8d6a572d1496d8010c6c11b236d51fc57
Kostas Kontaxis <kkontaxis@pm.me>
2021-01-07 18:49:50 +0000

    included picom.conf to install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/bash
 
-list=("alacritty/" "awesome/" "fish/" "nvim/" "rofi/")
+list=("alacritty/" "awesome/" "fish/" "nvim/" "rofi/" "picom.conf")
 for dir in ${list[@]}; do ln -s BetterThanYour/$dir ~/.config ; done
