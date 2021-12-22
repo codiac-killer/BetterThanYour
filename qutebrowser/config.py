85ef320f71249f18f6b15a4a5b28acd9deef488f
Kostas Kontaxis <kkontaxis@pm.me>
2021-12-23 00:58:22 +0200

    [add] qutebrowser : dvorak nav keybinds

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -19,6 +19,7 @@ c.url.searchengines = {
    'DEFAULT' : 'https://searx.be/search?q={}',
    'osm' : 'https://www.openstreetmap.org/search?query={}',
    'yewtube' : 'https://yewtu.be/search?q={}',
+   'invidious' : 'https://invidious.mutahar.rocks/search?q={}',
 }
 
 themes.spacethemedark.blood(c, {
@@ -35,3 +36,17 @@ c.tabs.padding = {"bottom": 10, "left": 5, "right": 5, "top": 10}
 c.tabs.title.format = '{audio}{current_title}'
 c.tabs.indicator.width = 0
 
+config.bind('h', 'scroll left')
+config.bind('t', 'scroll down')
+config.bind('n', 'scroll up')
+config.bind('s', 'scroll right')
+
+config.bind('H', 'back')
+config.bind('N', 'tab-prev')
+config.bind('T', 'tab-next')
+config.bind('S', 'forward')
+
+config.bind('l', 'search-next')
+config.bind('L', 'search-prev')
+
+config.bind('e', 'hint all')
